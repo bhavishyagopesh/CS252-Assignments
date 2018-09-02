eb67c64d94f78b0e9a04dbb990ec9a61367a4faa
raghukul01 <raghukul.raman01@gmail.com>
2018-09-02 18:22:31 +0530

    Removed Debug code

--- Assignment1/gen_html.sh
+++ Assignment1/gen_html.sh
@@ -17,10 +17,8 @@ for file_name in $( ls $folder ); do
 	# extract type of image
 	new_name=`echo $file_name | cut -f1 -d"."`
 	new_name=`echo $new_name | sed 's/.$//'`
-	
-	echo $new_name
+
 	if [ "$header" != "$new_name" ]; then
-		echo $header $new_name
 		echo "<tr><th>"$new_name"</th></tr>" >> $out_file
 		header="$new_name"
 	fi
